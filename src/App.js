5b589cbd749402f9bb6ba2718ee04c0000034433
towficaziz <tapolly01@gmail.com>
2023-09-18 00:09:48 +0200

    Adding error page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {
 import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
 import RootLayout from "./pages/Root";
+import ErrorPage from "./pages/Error";
 
 // const routeDefinitions = createRoutesFromElements(
 //   <Route>
@@ -20,9 +21,10 @@ const router = createBrowserRouter([
   {
     path: '/',
     element: <RootLayout />,
+    errorElement: <ErrorPage />,
     children: [
       { path: "/", element: <HomePage /> },
-      { path: "/products", element: <ProductsPage /> },
+      { path: "/products", element: <ProductsPage />},
     ]
   },
 ]);
